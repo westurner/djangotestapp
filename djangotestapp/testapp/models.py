1472a91c1c9b733d5074f9fe549824e257884eee
Wes Turner <wes@wrd.nu>
2016-07-14 22:02:45 -0500

    DOC: testapp/models.py: settings.AUTH_USER_MODEL because custom User models
     https://docs.djangoproject.com/en/1.9/topics/auth/customizing/#referencing-the-user-model

--- djangotestapp/testapp/models.py
+++ djangotestapp/testapp/models.py
@@ -1,9 +1,11 @@
 from __future__ import unicode_literals
 
+# from django.conf import settings
 from django.db import models
 
+
 class Message(models.Model):
-    # user = models.ForeignKey('auth.models.User', default=current_user)
+    # user = models.ForeignKey(settings.AUTH_USER_MODEL, default=current_user)
     user = models.TextField(db_index=True, max_length=42)
     message = models.TextField(db_index=True, max_length=140)
     dateCreated = models.DateTimeField(auto_now_add=True)
