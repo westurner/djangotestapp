50df3cf0bf435cafc66d722640d33347a18dfdca
Wes Turner <wes@wrd.nu>
2016-07-14 21:44:21 -0500

    ENH,TST: models.py, tests.py, migrations/0001_initial.py: create Message model, manage.py makemigrations, manage.py migrate

--- djangotestapp/testapp/models.py
+++ djangotestapp/testapp/models.py
@@ -2,4 +2,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Message(models.Model):
+    # user = models.ForeignKey('auth.models.User', default=current_user)
+    user = models.TextField(db_index=True, max_length=42)
+    message = models.TextField(db_index=True, max_length=140)
+    dateCreated = models.DateTimeField(auto_now_add=True)
+    dateModified = models.DateTimeField(auto_now=True)
+    starCount = models.IntegerField(default=0)
--- djangotestapp/testapp/tests.py
+++ djangotestapp/testapp/tests.py
@@ -8,3 +8,12 @@ class TestTestappSimpleViews(TestCase):
         r = c.get('/')
         self.assertContains(r, 'simple view')
         self.assertHTMLEqual(r.content, '<h1>simple view</h1>')
+
+from djangotestapp.testapp.models import Message
+class TestTestappModels(TestCase):
+    def test_models_0(self):
+        testdata = dict(user='abc', message='xyz')
+        m = Message(**testdata)
+        self.assertTrue(isinstance(m, Message))
+        self.assertEqual(m.user, testdata['user'])
+        self.assertEqual(m.message, testdata['message'])
