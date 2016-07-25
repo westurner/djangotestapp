3e48fedb1afa4264f69ed60836c94f3d05d8d31c
Wes Turner <wes@wrd.nu>
2016-07-25 02:27:50 -0500

    CLN: testapp/admin.py: fields on separate lines

--- djangotestapp/testapp/admin.py
+++ djangotestapp/testapp/admin.py
@@ -6,7 +6,21 @@ import djangotestapp.testapp.models as models
 @admin.register(models.Message)
 class MessageAdmin(admin.ModelAdmin):
     date_hierarchy = 'dateCreated'
-    fields = ('articleBody', 'user',
-              'dateCreated', 'likeCount', 'hashtags', 'users', 'articleBody_html')
-    readonly_fields = ('dateCreated', 'likeCount', 'hashtags', 'users', 'articleBody_html')
-    list_display = ('articleBody', 'user', 'dateCreated')
+    fields = (
+        'articleBody',
+        'user',
+        'dateCreated',
+        'likeCount',
+        'hashtags',
+        'users',
+        'articleBody_html')
+    readonly_fields = (
+        'dateCreated',
+        'likeCount',
+        'hashtags',
+        'users',
+        'articleBody_html')
+    list_display = (
+        'articleBody',
+        'user',
+        'dateCreated')
