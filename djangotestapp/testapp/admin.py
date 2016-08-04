f4e96336fd7e3d6c8b904ea89a3578c54d88c9de
Wes Turner <wes@wrd.nu>
2016-08-04 01:10:52 -0500

    ENH: testapp/admin.py: add HashtagAdmin

--- djangotestapp/testapp/admin.py
+++ djangotestapp/testapp/admin.py
@@ -3,6 +3,16 @@ from django.contrib import admin
 import djangotestapp.testapp.models as models
 
 
+@admin.register(models.Hashtag)
+class HashtagAdmin(admin.ModelAdmin):
+    fields = (
+        'name',)
+    readonly_fields = (
+        'name',)
+    list_display = (
+        'name',)
+
+
 @admin.register(models.Message)
 class MessageAdmin(admin.ModelAdmin):
     date_hierarchy = 'dateCreated'
